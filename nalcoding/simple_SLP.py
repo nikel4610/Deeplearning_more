15d25f49344c751fefb6fb6dff979a9fe43e5f3e
Hyun-woo <nikel4610@gmail.com>
2022-04-09 15:04:50 +0900

    def train_and_test

--- nalcoding/simple_SLP.py
+++ nalcoding/simple_SLP.py
@@ -29,6 +29,33 @@ def load_abalone_dataset():
         if row[0] == 'F' : data[n, 2] = 1
         data[n, 3: ] = row[1:]
 
+def init_model(): # weight 와 bias 를 초기화
+    global weight, bias, input_cnt, output_cnt
+    weight = np.randim.normal(RND_MEAD, RND_STD, [input_cnt, output_cnt])
+    bias = np.zeros([output_cnt])
+
+def train_and_test(epoch_count, mb_size, report):
+    step_count = arrange_data(mb_size)
+    test_x, test_y = get_test_data()
+
+    for epoch in range(epoch_count): # 에포크 수 만큼 학습 반복
+        losses, accs = [], []
+
+        for n in range(step_count): # step_count 만큼 미니배치 처리 반복
+            train_x, train_y = get_train_data(mb_size, n)
+            loss, acc = run_train(test_x, test_y)
+            losses.append(loss)
+            accs.append(acc)
+
+            if report > 0 and (epoch + 1) % report == 0: # 검사
+                acc = run_test(test_x, test_y)
+                print('Epoch {}: loss = {:5.3f}, acc = {:5.3f}/{:5.3f}'.format(epoch + 1,
+                                                                               np.mean(losses),np.mean(accs), acc))
+
+    final_acc = run_test(test_x, test_y)
+    print('\nFinal: final acc = {:5.3f}'.format(final_acc))
+
+
 def abaloine_exec(epoch_count = 10, mb_size = 10, report = 1):
     load_abalone_dataset()
     init_model()
