f7767bbbf808feb3ac31acff9d46d69eb12e860c
Hyun-woo <nikel4610@gmail.com>
2022-04-10 22:29:59 +0900

    backprop and forward postproc

--- nalcoding/pulsar.py
+++ nalcoding/pulsar.py
@@ -1,6 +1,22 @@
 import csv
 import numpy as np
 
+def forward_postproc(output, y): # 순전파 처리
+    entropy = sigmoid_cross_entropy_with_logits(y, output) # 시그모이드 교차 엔트로피
+    loss = np.mean(entropy) # 평균으로 loss값 계산
+    return loss, [y, output, entropy]
+
+def backdrop_postproc(G_loss, aux): # 역전파 처리
+    y, output, entropy = aux
+
+    g_loss_entropy = 1.0 / np.prod(entropy.shape) # 각 원소의 손실 기울기로 부여
+    g_entropy_output = sigmoid_cross_entropy_with_logits_derv(y, output)
+
+    G_entropy = g_loss_entropy * G_loss
+    G_output = g_entropy_output * G_entropy
+
+    return G_output
+
 def load_pulsar_dataset():
     with open("D:/python_project/nalcoding/Datasets/pulsar_data_test.csv") as csvfile:
         csvreader = csv.reader(csvfile)
