ac1b275f247d083f74230fa97064e192c25cb250
Hyun-woo <nikel4610@gmail.com>
2022-04-12 23:29:27 +0900

    redefinition forward, backprop postproc with softmax

--- nalcoding/softmax.py
+++ nalcoding/softmax.py
@@ -2,6 +2,22 @@ import simple_SLP
 import csv
 import numpy as np
 
+def backprop_postproc(G_loss, aux):
+    y, output, entropy = aux
+
+    g_loss_entropy = 1.0 / np.prod(entropy.shape)
+    g_entropy_output = softmax_cross_entropy_with_logits(y, output)
+
+    G_entropy = g_loss_entropy * G_loss
+    G_output = g_entropy_output * G_entropy
+
+    return G_output
+
+def forward_postproc(output, y):
+    entropy = softmax_cross_entropy_with_logits(y, output)
+    loss = np.mean(entropy)
+    return loss, [y, output, entropy]
+
 def load_steel_dataset():
     with open("D:/python_project/nalcoding/Datasets/faults.csv") as csvfile:
         csvreader = csv.reader(csvfile)
