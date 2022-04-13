4811d4d9fb58fdc96992e982117347902ab7e13b
Hyun-woo <nikel4610@gmail.com>
2022-04-13 22:08:31 +0900

    def backprop neuralnet hidden

--- nalcoding/MLP/simple_mlp.py
+++ nalcoding/MLP/simple_mlp.py
@@ -1,5 +1,32 @@
 import numpy as np
 
+def relu_derv(y):
+    return np.sign(y)
+
+def backprop_neuralnet_hidden(G_output, aux):
+    global pm_output, pm_hidden
+
+    x, hidden = aux
+
+    g_output_w_out = hidden.transpose()
+    G_w_out = np.matmul(g_output_w_out, G_output)
+    G_b_out = np.sum(G_output, axis = 0)
+
+    g_output_hidden = pm_output['w'].transpose()
+    G_hidden = np.matmul(G_output, g_output_hidden)
+
+    pm_output['w'] -= LEARNING_RATE * G_w_out
+    pm_output['b'] -= LEARNING_RATE * G_b_out
+
+    G_hidden = G_hidden * relu_derv(hidden)
+
+    g_hidden_w_hid = x.transpose()
+    G_w_hid = np.matmul(g_hidden_w_hid, G_hidden)
+    G_b_hid = np.sum(G_hidden, axis = 0)
+
+    pm_hidden['w'] -= LEARNING_RATE * G_w_hid
+    pm_hidden['b'] -= LEARNING_RATE * G_b_hid
+
 def relu (x):
     return np.maximum(x, 0)
 
