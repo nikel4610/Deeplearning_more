59a19226d8a071d8b0614884b26cd3e16f15a2a2
Hyun-woo <nikel4610@gmail.com>
2022-04-17 21:33:03 +0900

    add train and shuffle

--- nalcoding/MLP/dataset.py
+++ nalcoding/MLP/dataset.py
@@ -1,4 +1,5 @@
 import math_util
+import numpy as np
 
 class Dataset(object):
     def __init__(self, name, mode):
@@ -12,3 +13,20 @@ class Dataset(object):
     @property
     def train_count(self):
         return len(self.tr_xs)
+
+def dataset_get_train_data(self, batch_size, nth):
+    from_idx = nth * batch_size
+    to_idx = (nth + 1) * batch_size
+
+    tr_X = self.tr_xs[self.indices[from_idx:to_idx]]
+    tr_Y = self.tr_ys[self.indices[from_idx:to_idx]]
+
+    return tr_X, tr_Y
+
+def dataset_shuffle_train_data(self, size):
+    self.indices = np.arange(size)
+    np.random.shuffle(self.indices)
+
+Dataset.get_train_data = dataset_get_train_data
+Dataset.shuffle_train_data = dataset_shuffle_train_data
+
