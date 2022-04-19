e84140170bf8cef70a90935944e3611ebf2757bc
Hyun-woo <nikel4610@gmail.com>
2022-04-19 21:57:27 +0900

    add eval adam delta

--- nalcoding/MLP/adam_model.py
+++ nalcoding/MLP/adam_model.py
@@ -29,10 +29,32 @@ AdamModel.backprop_layer = adam_backprop_layer
 
 def adam_update_param(self, pm, key, delta):
     if self.use_adam:
+        # 파라미터의손실 기울기 전달
         delta = self.eval_adam_delta(pm, key, delta)
 
     pm[key] -= self.learning_rate * delta
 
 AdamModel.update_param = adam_update_param
 
+def adam_eval_adam_delta(self, pm, key, delta):
+    ro_1 = 0.9
+    ro_2 = 0.999
+    epsilon = 1.0e-8
+
+    skey, tkey, step = 's' + key, 't' + key, 'n' + key
+    if skey not in pm:
+        pm[skey] = np.zeros(pm[key].shape)
+        pm[tkey] = np.zeros(pm[key].shape)
+        pm[step] = 0
+
+    s = pm[skey] = ro_1 * pm[skey] + (1 - ro_1) * delta
+    t = pm[tkey] = ro_2 * pm[tkey] + (1 - ro_2) * (delta * delta)
+
+    pm[step] += 1
+    s = s / (1 - np.power(ro_1, pm[step]))
+    t = t / (1 - np.power(ro_2, pm[step]))
+
+    return s / (np.sqrt(t) + epsilon)
+
+AdamModel.eval_adam_delta = adam_eval_adam_delta
 
