32efbd3ee3474f18fca4afd4c995ba9f8b67184d
Hyun-woo <nikel4610@gmail.com>
2022-05-18 14:35:47 +0900

    html parser and prettify

--- web/webscraping.py
+++ web/webscraping.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
 # 웹 페이지 가져오기
 dangnReq = requests.get('https://www.daangn.com/hot_articles')
 # print(dangnReq.text) -> 전체 페이지 내용 출력
+
+dangnsoup = bs(dangnReq.content, 'html.parser')
+dangnsoup.prettify()
 
