b10415c5ca9fb6f5ee8ad26c07d9ce9d33d8db3b
Hyun-woo <nikel4610@gmail.com>
2022-07-18 11:16:05 +0900

    array

--- 빅데이터공부/0718.py
+++ 빅데이터공부/0718.py
@@ -70,44 +70,44 @@ import numpy as np
 # print(one_array)
 # print(one_array.dtype, one_array.shape)
 # ------------------------------------------------------
-array1 = np.arange(10)
-print('array1:\n', array1)
-# (2, 5) shape으로 변환
+# array1 = np.arange(10)
+# print('array1:\n', array1)
+# # (2, 5) shape으로 변환
 
-array2 = array1.reshape(2, 5)
-print('array2:\n',array2)
-#(5, 2) shape으로 변환.
+# array2 = array1.reshape(2, 5)
+# print('array2:\n',array2)
+# #(5, 2) shape으로 변환.
 
-array3 = array1.reshape(5,2)
-print('array3:\n',array3)
+# array3 = array1.reshape(5,2)
+# print('array3:\n',array3)
 
-# array1.reshape(4,3) # error
-array1 = np.arange(10)
-print(array1)
+# # array1.reshape(4,3) # error
+# array1 = np.arange(10)
+# print(array1)
 
-array2 = array1.reshape(-1,5)
-print('array2 shape:',array2.shape)
+# array2 = array1.reshape(-1,5)
+# print('array2 shape:',array2.shape)
 
-array3 = array1.reshape(5,-1)
-print('array3 shape:',array3.shape)
+# array3 = array1.reshape(5,-1)
+# print('array3 shape:',array3.shape)
 
-array1 = np.arange(10)
-# array4 = array1.reshape(-1,4) # error
-array1 = np.arange(8)
-array3d = array1.reshape((2,2,2))
-print('array3d:\n',array3d.tolist())
-# 3차원 ndarray를 2차원 ndarray로 변환하되 칼럼갯수는 1
+# array1 = np.arange(10)
+# # array4 = array1.reshape(-1,4) # error
+# array1 = np.arange(8)
+# array3d = array1.reshape((2,2,2))
+# print('array3d:\n',array3d.tolist())
+# # 3차원 ndarray를 2차원 ndarray로 변환하되 칼럼갯수는 1
 
-array5 = array3d.reshape(-1, 1)
-print('array5:\n',array5.tolist())
-print('array5 shape:',array5.shape)
-# 1차원 ndarray를 2차원 ndarray로 변환화되 칼럼 갯수는 1
+# array5 = array3d.reshape(-1, 1)
+# print('array5:\n',array5.tolist())
+# print('array5 shape:',array5.shape)
+# # 1차원 ndarray를 2차원 ndarray로 변환화되 칼럼 갯수는 1
 
-array6 = array1.reshape(-1, 1)
-print('array6:\n',array6.tolist())
-print('array6 shape:',array6.shape)
-# 3차원 array를 1차원으로 변환
+# array6 = array1.reshape(-1, 1)
+# print('array6:\n',array6.tolist())
+# print('array6 shape:',array6.shape)
+# # 3차원 array를 1차원으로 변환
 
-array1d = array3d.reshape(-1,)
-print(array1d)
+# array1d = array3d.reshape(-1,)
+# print(array1d)
 # ------------------------------------------------------
