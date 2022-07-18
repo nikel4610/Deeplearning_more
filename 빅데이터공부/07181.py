dd1b1c857e1df68fda1df71215e59423ce49e5de
Hyun-woo <nikel4610@gmail.com>
2022-07-18 11:35:43 +0900

    boolean indexing

--- 빅데이터공부/07181.py
+++ 빅데이터공부/07181.py
@@ -23,13 +23,40 @@ import numpy as np
 # print('(row=2,col=2) index 가리키는 값:', array2d[2,2] )
 # ------------------------------------------------------
 # 팬시 인덱싱 / 거의 안씀
-array1d = np.arange(start=1, stop=10)
-array2d = array1d.reshape(3,3)
-print(array2d)
-array3 = array2d[[0,1], 2]
-print('array2d[[0,1], 2] => ',array3.tolist())
-array4 = array2d[[0,1], 0:2]
-print('array2d[[0,1], 0:2] => ',array4.tolist())
-array5 = array2d[[0,1]]
-print('array2d[[0,1]] => ',array5.tolist())
+# array1d = np.arange(start=1, stop=10)
+# array2d = array1d.reshape(3,3)
+# print(array2d)
+# array3 = array2d[[0,1], 2]
+# print('array2d[[0,1], 2] => ',array3.tolist())
+# array4 = array2d[[0,1], 0:2]
+# print('array2d[[0,1], 0:2] => ',array4.tolist())
+# array5 = array2d[[0,1]]
+# print('array2d[[0,1]] => ',array5.tolist())
 # ------------------------------------------------------
+array1d = np.arange(start=1, stop=10)
+print(array1d)
+
+# [ ] 안에 array1d > 5 Boolean indexing을 적용
+array3 = array1d[array1d > 5]
+print('array1d > 5 불린 인덱싱 결과 값 :', array3)
+
+array1d > 5
+val = array1d > 5
+print(val, type(val), val.shape)
+
+boolean_indexes = np.array([False, False, False, False, False, True, True, True, True])
+array3 = array1d[boolean_indexes]
+print('불린 인덱스로 필터링 결과 :', array3)
+
+array1d = np.arange(start=1, stop=10)
+target = []
+#불린 인덱싱을 적용하지 않았을 경우
+for i in range(0, 9):
+    if array1d[i] > 5:
+        target.append(array1d[i])
+
+array_selected = np.array(target)
+print(array_selected)
+indexes = np.array([5,6,7,8])
+array4 = array1d[ indexes ]
+print('일반 인덱스로 필터링 결과 :',array4)
