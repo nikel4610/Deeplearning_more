034bafbe13aebd691b755e2f7618de481f748a58
Hyun-woo <nikel4610@gmail.com>
2022-07-19 14:38:57 +0900

    apply lambda

--- 빅데이터공부/0719/07192.py
+++ 빅데이터공부/0719/07192.py
@@ -67,32 +67,70 @@ titanic_df = pd.read_csv('titanic_train.csv')
 # )
 # )
 
-# 결손 데이터 처리 isna, fillna
-# 데이터처리에서 NaN값 처리는 필수!
-print(titanic_df.isna().head(3))
-print(titanic_df.isna( ).sum( )) # 각 col 별 NaN 개수 출력
+# # 결손 데이터 처리 isna, fillna
+# # 데이터처리에서 NaN값 처리는 필수!
+# print(titanic_df.isna().head(3))
+# print(titanic_df.isna( ).sum( )) # 각 col 별 NaN 개수 출력
 
-titanic_df['Cabin'] = titanic_df['Cabin'].fillna('C000') # NaN을 C000로 채움
-print(titanic_df.head(3))
+# titanic_df['Cabin'] = titanic_df['Cabin'].fillna('C000') # NaN을 C000로 채움
+# print(titanic_df.head(3))
 
-titanic_df['Age'] = titanic_df['Age'].fillna(titanic_df['Age'].mean())
-titanic_df['Embarked'] = titanic_df['Embarked'].fillna('S')
-print(titanic_df.isna().sum())
+# titanic_df['Age'] = titanic_df['Age'].fillna(titanic_df['Age'].mean())
+# titanic_df['Embarked'] = titanic_df['Embarked'].fillna('S')
+# print(titanic_df.isna().sum())
 
-print(titanic_df['Name'].value_counts())
+# print(titanic_df['Name'].value_counts())
 
-# nunique() : 유일한 값의 개수를 출력
-print(titanic_df['Pclass'].nunique())
-print(titanic_df['Survived'].nunique())
-print(titanic_df['Name'].nunique())
+# # nunique() : 유일한 값의 개수를 출력
+# print(titanic_df['Pclass'].nunique())
+# print(titanic_df['Survived'].nunique())
+# print(titanic_df['Name'].nunique())
 
-# replace로 원본 값 대체
-replace_test_df = pd.read_csv('titanic_train.csv')
+# # replace로 원본 값 대체
+# replace_test_df = pd.read_csv('titanic_train.csv')
 
-# Sex의 male값을 Man
-print(replace_test_df['Sex'].replace('male', 'Man'))
+# # Sex의 male값을 Man
+# print(replace_test_df['Sex'].replace('male', 'Man'))
 
-replace_test_df['Sex'] = replace_test_df['Sex'].replace({'male':'Man', 'female':'Woman'})
-replace_test_df['Cabin'] = replace_test_df['Cabin'].replace(np.nan, 'C001')
-print(replace_test_df['Cabin'].value_counts(dropna=False))
+# replace_test_df['Sex'] = replace_test_df['Sex'].replace({'male':'Man', 'female':'Woman'})
+# replace_test_df['Cabin'] = replace_test_df['Cabin'].replace(np.nan, 'C001')
+# print(replace_test_df['Cabin'].value_counts(dropna=False))
 
+# apply lambda 식으로 데이터 가공
+def get_square(a):
+    return a**2
+print('3의 제곱은:',get_square(3))
+
+lambda_square = lambda x : x ** 2
+print('3의 제곱은:',lambda_square(3))
+
+a=[1,2,3]
+squares = map(lambda x : x**2, a)
+print(list(squares))
+
+titanic_df['Name_len']= titanic_df['Name'].apply(lambda x : len(x))
+print(titanic_df[['Name','Name_len']].head(3))
+
+titanic_df['Child_Adult'] = titanic_df['Age'].apply(lambda x : 'Child' if x <=15 else 'Adult' )
+print(titanic_df[['Age','Child_Adult']].head(8))
+
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : 'Child' if x<=15 else ('Adult' if x <= 60 else 'Elderly'))
+print(titanic_df['Age_cat'].value_counts())
+
+# 나이에 따라 세분화된 분류를 수행하는 함수 생성. 
+def get_category(age):
+    cat = ''
+    if age <= 5: cat = 'Baby'
+    elif age <= 12: cat = 'Child'
+    elif age <= 18: cat = 'Teenager'
+    elif age <= 25: cat = 'Student'
+    elif age <= 35: cat = 'Young Adult'
+    elif age <= 60: cat = 'Adult'
+    else : cat = 'Elderly'
+ 
+    return cat
+
+# lambda 식에 위에서 생성한 get_category( ) 함수를 반환값으로 지정. 
+# get_category(X)는 입력값으로 ‘Age’ 칼럼 값을 받아서 해당하는 cat 반환
+titanic_df['Age_cat'] = titanic_df['Age'].apply(lambda x : get_category(x))
+print(titanic_df[['Age','Age_cat']].head())
