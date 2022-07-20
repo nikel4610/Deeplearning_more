9a48e2e09f0b5b98ab2135df71653256725f65b8
Hyun-woo <nikel4610@gmail.com>
2022-07-20 17:16:04 +0900

    simple naver stock crawling

--- 빅데이터공부/0720/07201.py
+++ 빅데이터공부/0720/07201.py
@@ -8,10 +8,21 @@ from bs4 import BeautifulSoup
 #     for item in items:
 #         print (item.get_text().strip())
 
+# res = requests.get('https://finance.naver.com/sise/')
+# html =res.content.decode('euc-kr','replace') # 인코딩 해야 한글로 나옴
+# soup = BeautifulSoup(html, 'html.parser')
+# # a 태그이면서 href 속성 값이 특정한 값을 갖는 경우 탐색
+# data = soup.select("#siselist_tab_0 > tr > td > a")
+# for item in data:
+#     print(item.get_text())
+
+import requests
+from bs4 import BeautifulSoup
 res = requests.get('https://finance.naver.com/sise/')
 html =res.content.decode('euc-kr','replace')
 soup = BeautifulSoup(html, 'html.parser')
-# a 태그이면서 href 속성 값이 특정한 값을 갖는 경우 탐색
-data = soup.select("#siselist_tab_0 > tr > td > a")
+data = soup.select("div.rgt > ul.lst_major > li")
+
 for item in data:
-    print(item.get_text())
+    # element 살펴보기
+    print ("지수이름:", item.find('a').get_text(), "현재지수:", item.find('span').get_text(), item.find('em').get_text())
