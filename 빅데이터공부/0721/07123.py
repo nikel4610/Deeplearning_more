198a6732d236d3a33b57b94e8ccf905d310ce538
Hyun-woo <nikel4610@gmail.com>
2022-07-21 14:03:27 +0900

    crawling test

--- 빅데이터공부/0721/07123.py
+++ 빅데이터공부/0721/07123.py
@@ -116,12 +116,33 @@ import apikeys
 
 client_id = apikeys.client_id
 client_secret = apikeys.client_secret
-naver_open_api = 'https://openapi.naver.com/v1/search/shop.json?query=무선충전기'
-header_params = {"X-Naver-Client-Id":client_id, "X-Naver-Client-Secret":client_secret}
-res = requests.get(naver_open_api, headers=header_params)
-if res.status_code == 200:
-    data = res.json()
-    for index, item in enumerate(data['items']):
-        print (index + 1, item['title'], item['link'])
-else:
-    print ("Error Code:", res.status_code)
+# naver_open_api = 'https://openapi.naver.com/v1/search/shop.json?query=무선충전기'
+# header_params = {"X-Naver-Client-Id":client_id, "X-Naver-Client-Secret":client_secret}
+# res = requests.get(naver_open_api, headers=header_params)
+# if res.status_code == 200:
+#     data = res.json()
+#     for index, item in enumerate(data['items']):
+#         print (index + 1, item['title'], item['link'])
+# else:
+#     print ("Error Code:", res.status_code)
+
+start, num = 1, 0
+excel_file = openpyxl.Workbook()
+excel_sheet = excel_file.active
+excel_sheet.column_dimensions['B'].width = 100
+excel_sheet.column_dimensions['C'].width = 100
+excel_sheet.append(['랭킹', '제목', '링크'])
+for index in range(10):
+    start_number = start + (index * 100)
+    naver_open_api = 'https://openapi.naver.com/v1/search/shop.json?query=무선충전기&display=100&start=' + str(start_number)
+    header_params = {"X-Naver-Client-Id":client_id, "X-Naver-Client-Secret":client_secret}
+    res = requests.get(naver_open_api, headers=header_params)
+    if res.status_code == 200:
+        data = res.json()
+        for item in data['items']:
+            num += 1
+            excel_sheet.append([num, item['title'], item['link']])
+    else:
+        print ("Error Code:", res.status_code)
+excel_file.save('IT.xlsx')
+excel_file.close()
