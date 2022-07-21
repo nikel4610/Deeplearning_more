dfc532802fee3aeccf2b96160da8ec3cdfa8ec84
Hyun-woo <nikel4610@gmail.com>
2022-07-21 10:40:57 +0900

    simple crawling with excel

--- 빅데이터공부/0721/0721.py
+++ 빅데이터공부/0721/0721.py
@@ -1,4 +1,7 @@
 import openpyxl
+import requests
+from bs4 import BeautifulSoup
+
 
 # excel_file = openpyxl.Workbook()
 # excel_sheet = excel_file.active
@@ -17,7 +20,7 @@ def write_excel_template(filename, sheetname, listdata):
     excel_file = openpyxl.Workbook()
     excel_sheet = excel_file.active
     # 글자 셀 길이 조절
-    excel_sheet.column_dimensions['A'].width = 100
+    excel_sheet.column_dimensions['A'].width = 80
     excel_sheet.column_dimensions['B'].width = 20
 
     if sheetname != '':
@@ -26,6 +29,27 @@ def write_excel_template(filename, sheetname, listdata):
     # 한 줄 씩 집어넣는다
     for item in listdata:
         excel_sheet.append(item)
-        
+
     excel_file.save(filename)
     excel_file.close()
+
+product_lists = list()
+
+for page_num in range(10):
+    if page_num == 0:
+        res = requests.get('https://davelee-fun.github.io/')
+    else:
+        res = requests.get('https://davelee-fun.github.io/page' + str(page_num + 1))
+    soup = BeautifulSoup(res.content, 'html.parser')
+    data = soup.select('div.card')
+
+# for 안에 for 넣는거 주의하기
+# 크롤링한 데이터를 한번 더 크롤링해서 저장한다
+    for item in data:
+        # > 랑 빈칸이랑 상관없음
+        product_name = item.select_one('div.card-body > h4')
+        product_date = item.select_one('div.wrapfooter span.post-date')
+        product_info = [product_name.get_text().strip(), product_date.get_text()]
+        product_lists.append(product_info)
+
+write_excel_template('tmp.xlsx', '상품정보', product_lists)
