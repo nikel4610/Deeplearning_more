1ca16bdacd9cb32c0ea893fc168d64582009435a
Hyun-woo <nikel4610@gmail.com>
2022-07-22 16:29:02 +0900

    xml practice

--- 빅데이터공부/0722/07223.py
+++ 빅데이터공부/0722/07223.py
@@ -1,9 +1,13 @@
-import csv
-data_file = open('빅데이터공부/0722/USvideos.csv', 'r', encoding='utf-8-sig')
-data_lines = csv.reader(data_file, delimiter=',')
+from bs4 import BeautifulSoup
 
-for data_line in data_lines:
-    print (data_line)
-    break
+data_file = open('빅데이터공부/0722/users.xml', 'r', encoding='utf-8-sig')
+soup = BeautifulSoup(data_file, 'lxml')
 
-data_file.close()
+users = soup.select('user')
+
+for user in users:
+    name = user.select_one('name')
+    print('이름:', name.text)
+    age = user.select_one('age')
+    print('나이:', age.text)
+    
