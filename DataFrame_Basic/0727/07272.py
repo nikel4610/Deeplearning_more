78099bdb170a5a4ab7a5c1d061c3ae95813f5053
nikel4610 <nikel4610@gmail.com>
2022-07-27 13:17:25 +0900

    add dataset

--- DataFrame_Basic/0727/07272.py
+++ DataFrame_Basic/0727/07272.py
@@ -6,7 +6,19 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/timeTest.csv')
 
 from datetime import datetime
-# 현재 날짜, 시간
-print(datetime.now())
-# 년 월 일 시 분 초
-print(datetime(1971, 1, 1, 12, 35, 55))
+# # 현재 날짜, 시간
+# print(datetime.now())
+# # 년 월 일 시 분 초
+# print(datetime(1971, 1, 1, 12, 35, 55))
+
+# # datetime 객체는 시간계산이 용이하다
+# t1 = datetime(1971, 1, 1, 12, 35, 55)
+# t2 = datetime(1971, 12, 12)
+# print(t2 - t1)
+# print(type(t1), type(t2)) # datetime.datetime
+# print(type(t2 - t1)) # timedelta
+
+# 날짜 시간 형식의 문자열 데이터는 시간 계산을 위해 datetime으로 변환해야 한다
+# -> to_datetime()
+
+
