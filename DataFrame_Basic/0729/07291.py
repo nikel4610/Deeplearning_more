5e5fe5ea991410f8d739a3036831939d60dba74f
nikel4610 <nikel4610@gmail.com>
2022-07-29 10:31:08 +0900

    korean law word cloud

--- DataFrame_Basic/0729/07291.py
+++ DataFrame_Basic/0729/07291.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import pandas as pd
 import numpy as np
+from collections import Counter
 
 # 한글 자연어 처리 (konlpy): 형태소 분석기(Hannanum, Kkma, Komoran, Mecab, Okt 포함)
 
@@ -18,4 +19,11 @@ with open('./lawkr.tst', 'r', encoding='utf-8') as f:
 
 # 명사만 추출
 nouns = okt.nouns(text)
+words = [word for word in nouns if len(word) > 1]
+c = Counter(words) # 단어별 빈도수 처리, dict타입으로 반환
 
+wc = WordCloud(font_path='C:/Windows/Fonts/malgun.ttf', width = 400, height = 400, scale = 2.0, max_font_size = 250)
+gen = wc.generate_from_frequencies(c)
+plt.figure()
+plt.imshow(gen)
+wc.to_file('./헌법_워드클라우드.png')
