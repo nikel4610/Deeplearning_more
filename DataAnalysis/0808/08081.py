7774c73fdf5fd439bf897c68069ddc8015a83f69
nikel4610 <nikel4610@gmail.com>
2022-08-08 11:02:35 +0900

    kaggle dataset

---
+++ DataAnalysis/0808/08081.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from  matplotlib import font_manager, rc
+import matplotlib.colors as mcolors
+import math
+import seaborn as sns
+
+font_path="C://Windows//Fonts//malgun.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+plt.rc('font', family=font)
+plt.rcParams['axes.unicode_minus'] = False
+
+data = pd.read_csv('./kaggle_survey_2020_responses.csv')
+# print(data.head())
