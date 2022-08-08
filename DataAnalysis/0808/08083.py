21802a277f0e81d7220de5b4d0e12fb12749e53e
nikel4610 <nikel4610@gmail.com>
2022-08-08 17:23:50 +0900

    covid practice

--- DataAnalysis/0808/08083.py
+++ DataAnalysis/0808/08083.py
@@ -14,6 +14,19 @@ plt.rcParams['axes.unicode_minus'] = False
 
 # https://github.com/CSSEGISandData/COVID-19
 
-doc = pd.read_csv('./COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports/04-01-2020.csv', encoding = 'utf-8')
+doc = pd.read_csv('./csse_covid_19_data/csse_covid_19_daily_reports/04-01-2020.csv', encoding = 'utf-8') # error_bad_lines = False -> 에러 무시)
 # print(doc.head())
+# print(doc.shape)
+# print(doc.columns)
 
+# plt.figure(figsize=(5,5))
+# sns.heatmap(data = doc.corr(), annot=True, fmt = '.2f', linewidths=0.5, cmap='Blues')
+# plt.show()
+
+countries = doc['Country_Region']
+covid_stat = doc[['Confirmed', 'Deaths', 'Recovered']]
+doc_us = doc[doc['Country_Region'] == 'US']
+doc = doc.dropna(subset=['Confirmed'])
+nan_data = {'Deaths': 0, 'Recovered':0}
+doc = doc.fillna(nan_data)
+# print(doc.head())
