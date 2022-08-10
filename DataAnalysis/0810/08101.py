77f0ec72f8b6a10c30def03db389baffd0f2ea4e
Hyun-woo <nikel4610@gmail.com>
2022-08-10 14:40:51 +0900

    add china

--- DataAnalysis/0810/08101.py
+++ DataAnalysis/0810/08101.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import os
 
-PATH = "./COVID-19-master/csse_covid_19_data/csse_covid_19_2022_us/"
+PATH = "./COVID-19-master/csse_covid_19_data/csse_covid_19_2022/"
 doc = pd.read_csv(PATH + "01-01-2022.csv", encoding='utf-8-sig')
 # print(doc.head())
 
@@ -10,6 +10,8 @@ country_info = pd.read_csv("./COVID-19-master/csse_covid_19_data/UID_ISO_FIPS_Lo
 
 def create_dateframe(filename):
     doc = pd.read_csv(PATH + filename, encoding='utf-8-sig') # 1. csv 파일 읽기
+    # China 컬럼만 추출
+    doc = doc[doc['Country_Region'] == 'China']
     doc = doc[['Province_State', 'Deaths']] # 2. 특정 컬럼만 선택해서 데이터프레임 만들기
     doc = doc.dropna(subset=['Deaths']) # 3. 특정 컬럼에 없는 데이터 삭제하기    
     doc = doc.astype({'Deaths': 'int64'}) # 5. 특정 컬럼의 데이터 타입 변경하기
@@ -44,4 +46,4 @@ def generate_dateframe_by_path(PATH):
 doc = generate_dateframe_by_path(PATH)
 # print(doc.head())
 doc = doc.astype('int64')
-doc.to_csv("./COVID-19-master/final_df_Deaths_us.csv")
+doc.to_csv("./COVID-19-master/final_df_Deaths_china.csv")
