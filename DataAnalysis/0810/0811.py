58487ffd19fed7872c5cbc5e308540029f064809
Hyun-woo <nikel4610@gmail.com>
2022-08-11 10:50:52 +0900

    tried recovered but not good

--- DataAnalysis/0810/0811.py
+++ DataAnalysis/0810/0811.py
@@ -3,17 +3,27 @@ import os
 
 seoul = pd.read_csv('./COVID-19-master\서울시 코로나19 확진자 발생동향.csv', encoding = 'cp949')
 
-seoul = seoul[['서울시 기준일', '서울시 확진자', '서울시 사망', '전국 확진', '전국 사망']]
-seoul = seoul.sort_values(by=['서울시 기준일'], ascending = True)
+# seoul = seoul[['서울시 기준일', '서울시 확진자', '서울시 사망', '전국 확진', '전국 사망']]
+# seoul = seoul.sort_values(by = ['서울시 기준일'], ascending = True)
 
-# seoul ratio
+# # seoul ratio
+# seoul['seoul_confirmed_ratio'] = seoul['서울시 확진자'] / seoul['전국 확진']
+# seoul['seoul_death_ratio'] = seoul['서울시 사망'] / seoul['전국 사망']
+
+# seoul = seoul.drop(seoul.index[0])
+
+# seoul.columns = ['date', 'seoul_confirmed', 'seoul_death', 'korea_confirmed', 'korea_death', 'seoul_confirmed_ratio', 'seoul_death_ratio']
+# seoul.to_csv('./COVID-19-master/seoul_confirmed_ratio.csv')
+
+seoul = seoul.sort_values(by = ['서울시 기준일'], ascending = True)
+
 seoul['seoul_confirmed_ratio'] = seoul['서울시 확진자'] / seoul['전국 확진']
 seoul['seoul_death_ratio'] = seoul['서울시 사망'] / seoul['전국 사망']
+seoul['seoul_recovered_ratio'] = seoul['서울시 퇴원'] / seoul['전국 퇴원']
 
-seoul = seoul.drop(seoul.index[0])
+seoul = seoul[['서울시 기준일', 'seoul_confirmed_ratio', 'seoul_death_ratio', 'seoul_recovered_ratio']]
 
-seoul.columns = ['date', 'seoul_confirmed', 'seoul_death', 'korea_confirmed', 'korea_death', 'seoul_confirmed_ratio', 'seoul_death_ratio']
-seoul.to_csv('./COVID-19-master/seoul_confirmed_ratio.csv')
+seoul.columns = ['date', 'seoul_confirmed_ratio', 'seoul_death_ratio', 'seoul_recovered_ratio']
+seoul['seoul_recovered_ratio'].replace('', 0, inplace = True)
 
-
-
+seoul.to_csv('./COVID-19-master/seoul_ratio.csv')
